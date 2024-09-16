2137519a5eecf21c4c9cfe42a3d5bce8ad53e114
Jonas Santos <jonas.santos2302@outlook.com>
2024-09-16 11:32:40 -0400

    Salvar Dados para Excel

--- Extracao-de-dados-pdf/bot.py
+++ Extracao-de-dados-pdf/bot.py
@@ -1,84 +1,102 @@
-"""
-WARNING:
-
-Please make sure you install the bot dependencies with `pip install --upgrade -r requirements.txt`
-in order to get all the dependencies on your Python environment.
-
-Also, if you are using PyCharm or another IDE, make sure that you use the SAME Python interpreter
-as your IDE.
-
-If you get an error like:
-```
-ModuleNotFoundError: No module named 'botcity'
-```
-
-This means that you are likely using a different Python interpreter than the one used to install the dependencies.
-To fix this, you can either:
-- Use the same interpreter as your IDE and install your bot with `pip install --upgrade -r requirements.txt`
-- Use the same interpreter as the one used to install the bot (`pip install --upgrade -r requirements.txt`)
-
-Please refer to the documentation for more information at
-https://documentation.botcity.dev/tutorials/python-automations/web/
-"""
-
-
-# Import for the Web Bot
-from botcity.web import WebBot, Browser, By
-
-# Import for integration with BotCity Maestro SDK
+from botcity.core import DesktopBot
+from botcity.plugins.email import BotEmailPlugin
 from botcity.maestro import *
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-# Disable errors if we are not connected to Maestro
+import PyPDF2
+
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
+def ler_pdf(caminho_pdf):
+    texto_pdf = []
+    try:
+        with open(caminho_pdf, 'rb') as arquivo:
+            leitor = PyPDF2.PdfReader(arquivo)
+            num_paginas = len(leitor.pages)
+            for i in range(num_paginas):
+                pagina = leitor.pages[i]
+                texto = pagina.extract_text()
+                texto_pdf.append(texto)
+    except Exception as e:
+        print(f"Erro ao ler o PDF: {e}")
+    return texto_pdf
+
+def analisar_texto(texto_pdf):
+    resultado = []
+    for texto in texto_pdf:
+        linhas = texto.strip().split('\n')
+        linhas = linhas[1:]
+        print(linhas)
+        for linha in linhas:
+            partes = linha.split()
+            if len(partes) < 3:
+                print(f"Linha com formato inesperado: {linha}")
+                continue
+            cpf = partes[-2]
+            status = partes[-1]
+            nome = ' '.join(partes[:-2])
+
+            if status == 'Não':
+                resultado.append({'Nome': nome, 'CPF': cpf, 'Status': status})
+
+    return resultado
+
+def salvar_em_excel(dados, caminho_excel):
+    try:
+        df = pd.DataFrame(dados)
+        df.to_excel(caminho_excel, index=False)
+        print(f"Dados salvos em {caminho_excel}")
+    except Exception as e:
+        print(f"Erro ao salvar em Excel: {e}")
+
+def processar_pdf(caminho_pdf, caminho_excel):
+    textos = ler_pdf(caminho_pdf)
+    dados = analisar_texto(textos)
+    salvar_em_excel(dados, caminho_excel)
+    
+def enviar_email(user_email, user_senha, to_email, assunto, conteudo, arquivo_path ):
+    email = BotEmailPlugin()
+    email.configure_imap("imap.gmail.com", 993)
+    email.configure_smtp("smtp.gmail.com", 587)
+    email.login(user_email, user_senha)
+    email.send_message(assunto, conteudo, to_email, attachments=arquivo_path, use_html=True)
+    email.disconnect()
+
+    print("E-mail enviado com sucesso!")
+    
+def parametro_emails():
+
+    to = ["sabrina.frazao@ifam.edu.br", "sabrinadasilvafrazao@gmail.com"]
+    subject = "Relatorio_SUS"
+    body = '''Bom dia! 
+            Segue em anexo o relatorio das pessoas que não possuem número SUS'''
+    files = ["Dados.xlsx"]
+    
+    enviar_email(
+    user_email="jonas.santos2302@gmail.com",
+    user_senha="aaaaaaaaaaaaa",
+    to_email=to,
+    assunto=subject,
+    conteudo=body,
+    arquivo_path=files
+    )
 
 def main():
-    # Runner passes the server url, the id of the task being executed,
-    # the access token and the parameters that this task receives (when applicable).
     maestro = BotMaestroSDK.from_sys_args()
-    ## Fetch the BotExecution with details from the task, including parameters
-    execution = maestro.get_execution()
+    execucao = maestro.get_execution()
 
-    print(f"Task ID is: {execution.task_id}")
-    print(f"Task Parameters are: {execution.parameters}")
+    print(f"ID da Tarefa é: {execucao.task_id}")
+    print(f"Parâmetros da Tarefa são: {execucao.parameters}")
 
-    bot = WebBot()
+    bot = DesktopBot()
+    bot.browse("http://www.botcity.dev")
 
-    # Configure whether or not to run on headless mode
-    bot.headless = False
-
-    # Uncomment to change the default Browser to Firefox
-    bot.browser = Browser.CHROME
-
-    # Uncomment to set the WebDriver path
-    bot.driver_path = ChromeDriverManager().install()
-
-    # Opens the BotCity website.
-    #bot.browse("https://www.botcity.dev")
-
-    # Implement here your logic...
-    ...
-
-    # Wait 3 seconds before closing
-    bot.wait(3000)
-
-    # Finish and clean up the Web Browser
-    # You MUST invoke the stop_browser to avoid
-    # leaving instances of the webdriver open
-    bot.stop_browser()
-
-    # Uncomment to mark this task as finished on BotMaestro
-    # maestro.finish_task(
-    #     task_id=execution.task_id,
-    #     status=AutomationTaskFinishStatus.SUCCESS,
-    #     message="Task Finished OK."
-    # )
-
-
+    caminho_pdf = 'Controle_SUS.pdf'
+    caminho_excel = 'Dados.xlsx'
+    processar_pdf(caminho_pdf, caminho_excel)
+    parametro_emails()
+    
 def not_found(label):
     print(f"Element not found: {label}")
-
 
 if __name__ == '__main__':
     main()
